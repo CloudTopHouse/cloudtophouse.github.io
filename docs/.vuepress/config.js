0e5b01df41dd7275003e872877ee99d988506845
RyuZheng <zhengxiaochuan@miaoyouche.com>
2019-12-04 13:57:15 +0800

    update:添加vue相关文档;

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -285,7 +285,10 @@ module.exports = {
 				        {
 				        	title: 'Vue',
 				        	children: [
-				              '/zh/Frontend/Vue/01-Vue介绍',
+				              '/zh/Frontend/Vue/01-了解前端知识体系',
+				              '/zh/Frontend/Vue/02-了解前后分离的演变史',
+				              '/zh/Frontend/Vue/03-了解前端 MVVM 模式',
+				              '/zh/Frontend/Vue/04-Vue 简介',
 				          	]
 				        }
 				    ],
